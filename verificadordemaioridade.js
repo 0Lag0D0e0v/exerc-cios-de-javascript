765455be0da5f0d700274af4279af6a3cbc6fa98
0Lag0D0e0v <luisandregeraldo@yahoo.com.br>
2025-11-10 11:43:00 -0300

    Esse trecho de código verifica a condição nota.

--- verificadordemaioridade.js
+++ verificadordemaioridade.js
@@ -1,8 +1,20 @@
-let idade= 18;
+//let idade= 18;
 
-if(idade >= 18){
-    console.log("Você é maior de idade !");
+//if(idade >= 18){
+    //console.log("Você é maior de idade !");
+//}
+ //else{
+    //console.log("Você não é maior de idade !");
+//}
+
+let nota= 3;
+
+if(nota >= 9){
+    console.log("Excelente !");
+}
+ else if (nota>=6){
+    console.log("Você foi Aprovado!");
 }
  else{
-    console.log("Você não é maior de idade !");
-}
+    console.log("Você foi Reprovado!")
+ }
